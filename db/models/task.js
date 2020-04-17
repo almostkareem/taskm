d5bc7eca77fecf8d57fc43abfc1e9581db218374
capcashmere <capcashmere@gmail.com>
2020-04-17 12:14:28 +0100

    added timestamps to db model

--- db/models/task.js
+++ db/models/task.js
@@ -1,33 +1,43 @@
 const Sequelize = require('sequelize')
-const {DB_DEFATUL_TYPE} = require('../../const')
+const { DB_DEFATUL_TYPE } = require('../../const')
 
 module.exports = (sequelize, type) => {
   return sequelize.define('task', {
-      id: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV1,
-        primaryKey: true,
-      },
-      sourceId: {
-        type: type.STRING,
-        allowNull: false
-      },
-      type: {
-        type: type.STRING,
-        defaultValue: DB_DEFATUL_TYPE,
-        allowNull: false
-      },      
-      title: {
-        type: type.STRING,
-        allowNull: true
-      },
-      meta: {
-        type: type.STRING,
-        allowNull: true,
-      },
-      status: {
-        type: type.STRING,
-        allowNull: false,
-      }
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV1,
+      primaryKey: true,
+    },
+    sourceId: {
+      type: type.STRING,
+      allowNull: false
+    },
+    type: {
+      type: type.STRING,
+      defaultValue: DB_DEFATUL_TYPE,
+      allowNull: false
+    },
+    title: {
+      type: type.STRING,
+      allowNull: true
+    },
+    meta: {
+      type: type.STRING,
+      allowNull: true,
+    },
+    status: {
+      type: type.STRING,
+      allowNull: false,
+    },
+    created_at: {
+      type: 'TIMESTAMP',
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      allowNull: false
+    },
+    updated_at: {
+      type: 'TIMESTAMP',
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      allowNull: false
+    }
   })
 }
